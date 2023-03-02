8fe76abf2abd91bbff03bab4993894bf987b94e4
ValerioGambino <valerio.gambino.web@gmail.com>
2023-03-02 12:21:10 +0100

    Fine lezione 22, panoramica su cosa sono e come funzionano i component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root', //stringa usata nel file index.html dal quale comprende cosa utilizzare
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h3 {
+    color: dodgerblue;
+  }
+  `]
 })
 export class AppComponent {
 }
