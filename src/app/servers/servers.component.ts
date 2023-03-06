36998b6d8926fbe981015bafb2e97a0b2b85296d
ValerioGambino <valerio.gambino.web@gmail.com>
2023-03-06 21:43:36 +0100

    Fine lezione 28 - Property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
   // selector: './app-servers',
   selector: 'app-servers',
-  template: `
+  /*template: `
     <app-server></app-server>
-    <app-server></app-server>`,
+    <app-server></app-server>`,*/
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    },2000);
+  }
+
+  ngOnInit() {
+  }
 
 }
